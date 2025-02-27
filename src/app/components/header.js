f3a1fdef8f44b6af92c9c822de54cde74daeae61
Huckdj <manhduc132003@gmail.com>
2025-02-27 10:06:01 +0700

    no message

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -110,7 +110,7 @@ export default function HeaderPage() {
         )}
       </div>
 
-      <div className=" flex lg:hidden max-h-32 container justify-between mx-auto bg-white/75 border rounded-full p-3 mt-4">
+      <div className=" flex lg:hidden min-h-12 max-h-12 container justify-between mx-auto bg-white/75 border rounded-full p-3 mt-4">
         <img
           src="https://res.cloudinary.com/dumx42hqq/image/upload/v1740054410/Screenshot_2025-02-20_191821-removebg-preview_ylvnge.png"
           className="w-28"
